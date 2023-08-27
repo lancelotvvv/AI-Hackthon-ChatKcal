354cbb5857d878cf3d339ad41715b162574e7888
HF LIU <58149742+lancelotvvv@users.noreply.github.com>
2023-08-27 13:30:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,119 @@
-import os
+
+# %%
 import streamlit as st
+import os
+from tabulate import tabulate
+import json
+from langchain.chains import LLMChain
 from langchain.llms import OpenAI
-from langchain import PromptTemplate, LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+import pandas as pd
 # from langchain.memory import ConversationBufferMemory
+os.environ['OPENAI_API_KEY'] = "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"
 
 st.title('''⭐🍇🍉🍍ChatKcal🥭🍎🥝⭐
           🍽️ 🌿Your Personal AI Nutritionist Assitant
           ''')
 
+
 # openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-def generate_response(prompt,age_text,weight_text,height_text,activity_level,plan_type):
-    llm = OpenAI(temperature=0,max_tokens=2000)
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
-    # st.info(llm(prompt.format(weight_text=weight_text,age_text=age_text)))
-    st.info(llm_chain.run(weight_text=weight_text, 
-                          age_text=age_text,
-                          height_text=height_text,
-                          activity_level=activity_level,
-                          plan_type=plan_type))
+def generate_response(age_text, weight_text, height_text, activity_level, plan_type):
+    llm = OpenAI(temperature=0, max_tokens=2000)
+    # Notice that "chat_history" is present in the prompt template
+    template = """You are to create a meal plan
+
+            Previous conversation:
+            {chat_history}
+
+            New human question: {question}
+            Response:"""
+    prompt = PromptTemplate.from_template(template)
+    # Notice that we need to align the `memory_key`
+    # To not return the entire history, set verbose=False
+    memory = ConversationBufferMemory(memory_key="chat_history")
+
+    answer_format = '''
+            {
+                "reply": "",
+                "menu": {
+                    "Breakfast": [
+                        {
+                            "Dish": "dish name",
+                            "Ingredients": [
+                                ["ingredients_name", "quantity", "calories"],
+                                ["ingredients_name", "quantity", "calories"]
+                            ],
+                            "Total_calories": ""
+                        }
+                    ],
+                    .....
+                }
+            }
+            '''
+
+    reply = '''
+            {
+                "reply": "I understand",
+                "menu": {
+                    "Breakfast": [],
+                    "Lunch":[],
+                    "Dinner":[]
+                }
+            }
+            '''
+    memory.save_context({"input": "All your responses should only be in a json file like the following, "
+                                  "put your response into the reply section. the menu secotion should be a meal plan"
+                                  "with the calories calculation :\n" + answer_format +
+                                  " respond I understand if you understand"},
+                        {"output": reply})
+    conversation = LLMChain(
+        llm=llm,
+        prompt=prompt,
+        verbose=True,
+        memory=memory
+    )
+    answer = conversation(f'''
+            Questions:
+            For a person: weight of {weight_text}kg, age of {age_text} years old, height of {height_text}cm and {activity_level} Level. 
+            please design a detailed {plan_type} including calories breakdown for this person.
+            ''')
+
+
+
+
+    sample_reply = json.loads(answer["text"])
+
+    # # Initialize empty lists to store the data
+    # rows = []
+    #
+    # # Loop through the menu items and their details
+    #
+    # for meal, dishes in sample_reply['menu'].items():
+    #     for dish in dishes:
+    #         ingredients_list = ", ".join([ingredient[0] for ingredient in dish['Ingredients']])
+    #         total_calories_range = dish['Total_calories']
+    #         rows.append([meal, dish['Dish'], ingredients_list, total_calories_range])
+    #
+    # # Create a DataFrame
+    # df = pd.DataFrame(rows, columns=['Meal', 'Dish', 'Ingredients', 'Total_calories'])
+    # st.table(df)
+
+    for meal, dishes in sample_reply["menu"].items():
+        st.text(meal)
+
+        table_data = []
+        for item in dishes:
+            dish = item["Dish"]
+            total_calories = item["Total_calories"]
+            ingredients = "\n".join([f"{ing[0]} ({ing[1]}) - {ing[2]}" for ing in item["Ingredients"]])
+            table_data.append([dish, ingredients, total_calories])
+
+        # Create a card-like display
+        table = tabulate(table_data, headers=["Dish", "Ingredients", "Total Calories"], tablefmt="grid")
+
+        # Print the card-like display
+        st.text(table)
 
 
 with st.form('my_form'):
@@ -25,19 +121,19 @@ with st.form('my_form'):
     age_text = st.slider('How old are you?', 0, 130, 25)
     weight_text = st.slider("What's your weight in kg?", 0, 200, 60)
     height_text = st.slider("What's your height in cm?", 0, 300, 165)
-    col1, col2= st.columns(2)
+    col1, col2 = st.columns(2)
     with col1:
-            activity_level = st.radio(
-                "What is your Daily Activity Level 🤸",
-                key="activity",
-                options=['Light Activity', 'Moderate Activity', 'Vigorous Activity'],
-            ) 
+        activity_level = st.radio(
+            "What is your Daily Activity Level 🤸",
+            key="activity",
+            options=['Light Activity', 'Moderate Activity', 'Vigorous Activity'],
+        )
     with col2:
-            plan_type = st.radio(
-                "What do you need 🍔",
-                key="plan",
-                options=['Daily Meal Plan', 'Nutrition Plan'],
-            )
+        plan_type = st.radio(
+            "What do you need 🍔",
+            key="plan",
+            options=['Daily Meal Plan', 'Nutrition Plan'],
+        )
 
     # activity_level = st.selectbox(
     # 'What is your Daily Activity Level',
@@ -54,50 +150,38 @@ with st.form('my_form'):
     #     options=['Nutrition Plan', 'Daily Meal Plan'],
     # )
     submitted = st.form_submit_button('Generate your plan')
-    if  age_text=='':
+    if age_text == '':
         st.warning('Please choose your age', icon='⚠')
-    elif  weight_text=='':
+    elif weight_text == '':
         st.warning('Please choose your weight', icon='⚠')
-    elif  height_text=='':
+    elif height_text == '':
         st.warning('Please choose your height', icon='⚠')
-    elif  activity_level=='':
+    elif activity_level == '':
         st.warning('Please choose your activity level', icon='⚠')
-    elif  plan_type=='':
+    elif plan_type == '':
         st.warning('Please choose your plan type', icon='⚠')
-    elif submitted and os.environ['OPENAI_API_KEY'].startswith('sk-'):        
+    elif submitted and os.environ['OPENAI_API_KEY'].startswith('sk-'):
         # template = '''
-        # Questions: 
-        # Calculate Daily calorie intake by using 
-        # Equation 1: Basal Metabolic Rate (BMR) = 10 x Weight (kg) + 6.25 x Height (cm) - 5 x Age (years) + 5, 
+        # Questions:
+        # Calculate Daily calorie intake by using
+        # Equation 1: Basal Metabolic Rate (BMR) = 10 x Weight (kg) + 6.25 x Height (cm) - 5 x Age (years) + 5,
         # and Equation 2: Daily calorie intake = BMR x Activity Level
-        # Based on 2023 nutriention table and daily calorie intake result from previous step, 
+        # Based on 2023 nutriention table and daily calorie intake result from previous step,
         # please create a nutrition plan for a person with following infomation:
-        # weight of {weight_text}kg, age of {age_text} years old, height of {height_text}cm and {activity_level} Level. 
+        # weight of {weight_text}kg, age of {age_text} years old, height of {height_text}cm and {activity_level} Level.
         # Only return the final detailed meal plan in a table format, break down into calories, make sure starting with my weight and age and my colaries intake
         # '''
 
         # template = '''
-        # Questions: 
-        # Calculate Daily calorie intake by using 
-        # Equation 1: Basal Metabolic Rate (BMR) = 10 x Weight (kg) + 6.25 x Height (cm) - 5 x Age (years) + 5, 
+        # Questions:
+        # Calculate Daily calorie intake by using
+        # Equation 1: Basal Metabolic Rate (BMR) = 10 x Weight (kg) + 6.25 x Height (cm) - 5 x Age (years) + 5,
         # and Equation 2: Daily calorie intake = BMR x Activity Level
-        # Based on 2023 nutriention table and daily calorie intake result from previous step, 
+        # Based on 2023 nutriention table and daily calorie intake result from previous step,
         # please create a nutrition plan for a person with following infomation:
-        # weight of {weight_text}kg, age of {age_text} years old, height of {height_text}cm and {activity_level} Level. 
+        # weight of {weight_text}kg, age of {age_text} years old, height of {height_text}cm and {activity_level} Level.
         # Only return the final detailed meal plan in a table format, break down into calories, make sure starting with my weight and age and my colaries intake
         # Answer: Let's think step by step.
         # '''
 
-        template = '''
-        Questions:
-        For a person: weight of {weight_text}kg, age of {age_text} years old, height of {height_text}cm and {activity_level} Level. 
-        Step 1: Calculate BMR using Equation 1: Basal Metabolic Rate (BMR) = 10 x Weight (kg) + 6.25 x Height (cm) - 5 x Age (years) + 5
-        Step 2: Calculate Daily Calorie Intake using BMR from step 1 and Equation 2: Daily calorie intake = BMR x Activity Level
-        Step 3: Based on 2023 nutriention table and daily calorie intake result from previous step, 
-        Answer: Let's think step by step.
-        please design a detailed {plan_type} including calories breakdown for this person.
-        '''
-
-        prompt = PromptTemplate(template=template, input_variables=['age_text','weight_text','height_text','activity_level','plan_type'])
-        # print(prompt)
-        generate_response(prompt,age_text,weight_text,height_text,activity_level,plan_type)
+        generate_response(age_text, weight_text, height_text, activity_level, plan_type)
